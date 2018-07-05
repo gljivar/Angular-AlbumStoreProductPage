dadf774c35a09bb2011072fc2882beef9e8a9dd1
Miroslav V <miroslav.vrbanac@gmail.com>
2018-07-05 23:12:23 +0200

    Change order of routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,9 +12,9 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { Routes, RouterModule } from '@angular/router';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: 'products', pathMatch: 'full' },
   { path: 'products', component: ProductListComponent },
-  { path: 'product/:id', component: ProductPageComponent }
+  { path: 'product/:id', component: ProductPageComponent },
+  { path: '', redirectTo: 'products', pathMatch: 'full' }
 ];
 
 @NgModule({
